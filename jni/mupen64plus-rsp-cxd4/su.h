18216536d09a16bc86c8108d988f9d1271ad2ce2
Paul Lamb <paul@paulscode.com>
2015-01-02 12:03:20 -0600

    rsp-cxd4: Add source files

---
+++ jni/mupen64plus-rsp-cxd4/su.h
@@ -0,0 +1,364 @@
+/******************************************************************************\
+* Project:  Basic MIPS R4000 Instruction Set for Scalar Unit Operations        *
+* Authors:  Iconoclast                                                         *
+* Release:  2014.12.25                                                         *
+* License:  CC0 Public Domain Dedication                                       *
+*                                                                              *
+* To the extent possible under law, the author(s) have dedicated all copyright *
+* and related and neighboring rights to this software to the public domain     *
+* worldwide. This software is distributed without any warranty.                *
+*                                                                              *
+* You should have received a copy of the CC0 Public Domain Dedication along    *
+* with this software.                                                          *
+* If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.             *
+\******************************************************************************/
+
+#ifndef _SU_H_
+#define _SU_H_
+
+#include <stdio.h>
+#include "my_types.h"
+#include "rsp.h"
+
+#define EXTERN_COMMAND_LIST_GBI
+#define EXTERN_COMMAND_LIST_ABI
+#define SEMAPHORE_LOCK_CORRECTIONS
+#define WAIT_FOR_CPU_HOST
+
+#if (0)
+#define SP_EXECUTE_LOG
+#define VU_EMULATE_SCALAR_ACCUMULATOR_READ
+#endif
+
+#define RSP_INFO_NAME           RSP_info
+#define GET_RSP_INFO(member)    (RSP_INFO_NAME.member)
+#define GET_RCP_REG(member)     (*RSP_INFO_NAME.member)
+
+extern int CPU_running;
+
+extern RSP_INFO RSP_INFO_NAME;
+extern pu8 DRAM;
+extern pu8 DMEM;
+extern pu8 IMEM;
+
+extern u8 conf[32];
+
+/*
+ * RSP virtual registers (of scalar unit)
+ * The most important are the 32 general-purpose scalar registers.
+ * We have the convenience of using a 32-bit machine (Win32) to emulate
+ * another 32-bit machine (MIPS/N64), so the most natural way to accurately
+ * emulate the scalar GPRs is to use the standard `int` type.  Situations
+ * specifically requiring sign-extension or lack thereof are forcibly
+ * applied as defined in the MIPS quick reference card and user manuals.
+ * Remember that these are not the same "GPRs" as in the MIPS ISA and totally
+ * abandon their designated purposes on the master CPU host (the VR4300),
+ * hence most of the MIPS names "k0, k1, t0, t1, v0, v1 ..." no longer apply.
+ */
+extern i32 SR[32];
+
+#define FIT_IMEM(PC)    (PC & 0xFFF & 0xFFC)
+
+#ifdef EMULATE_STATIC_PC
+#define CONTINUE    continue
+#define JUMP        goto BRANCH
+#else
+#define CONTINUE    break
+#define JUMP        break
+#endif
+
+#ifdef EMULATE_STATIC_PC
+#define BASE_OFF    0x000
+#else
+#define BASE_OFF    0x004
+#endif
+
+#ifndef EMULATE_STATIC_PC
+int stage;
+#endif
+
+extern int temp_PC;
+#ifdef WAIT_FOR_CPU_HOST
+extern short MFC0_count[32];
+/* Keep one C0 MF status read count for each scalar register. */
+#endif
+
+#define SLOT_OFF    (BASE_OFF + 0x000)
+#define LINK_OFF    (BASE_OFF + 0x004)
+extern void set_PC(int address);
+
+#if (0x7FFFFFFFul >> 037 != 0x7FFFFFFFul >> ~0U)
+#define MASK_SA(sa) (sa & 037)
+/* Force masking in software. */
+#else
+#define MASK_SA(sa) (sa)
+/* Let hardware architecture do the mask for us. */
+#endif
+
+#define SR_B(s, i)      (*(pi8)(((pi8)(SR + s)) + BES(i)))
+#define SR_S(s, i)      (*(pi16)(((pi8)(SR + s)) + HES(i)))
+#define SE(x, b)        (-(x & (1 << b)) | (x & ~(~0 << b)))
+#define ZE(x, b)        (+(x & (1 << b)) | (x & ~(~0 << b)))
+
+/*
+ * Since RSP vectors are stored 100% accurately as big-endian arrays for the
+ * proper vector operation math to be done, LWC2 and SWC2 emulation code will
+ * have to look a little different.  zilmar's method is to distort the endian
+ * using an array of unions, permitting hacked byte- and halfword-precision.
+ */
+
+/*
+ * Universal byte-access macro for 16*8 halfword vectors.
+ * Use this macro if you are not sure whether the element is odd or even.
+ */
+#define VR_B(vt,element)    (*(pi8)((pi8)(VR[vt]) + MES(element)))
+
+/*
+ * Optimized byte-access macros for the vector registers.
+ * Use these ONLY if you know the element is even (or odd in the second).
+ */
+#define VR_A(vt,element)    (*(pi8)((pi8)(VR[vt]) + element + MES(0x0)))
+#define VR_U(vt,element)    (*(pi8)((pi8)(VR[vt]) + element - MES(0x0)))
+
+/*
+ * Optimized halfword-access macro for indexing eight-element vectors.
+ * Use this ONLY if you know the element is even, not odd.
+ *
+ * If the four-bit element is odd, then there is no solution in one hit.
+ */
+#define VR_S(vt,element)    (*(pi16)((pi8)(VR[vt]) + element))
+
+/*** Scalar, Coprocessor Operations (system control) ***/
+#define SP_STATUS_HALT          (0x00000001 <<  0)
+#define SP_STATUS_BROKE         (0x00000001 <<  1)
+#define SP_STATUS_DMA_BUSY      (0x00000001 <<  2)
+#define SP_STATUS_DMA_FULL      (0x00000001 <<  3)
+#define SP_STATUS_IO_FULL       (0x00000001 <<  4)
+#define SP_STATUS_SSTEP         (0x00000001 <<  5)
+#define SP_STATUS_INTR_BREAK    (0x00000001 <<  6)
+#define SP_STATUS_SIG0          (0x00000001 <<  7)
+#define SP_STATUS_SIG1          (0x00000001 <<  8)
+#define SP_STATUS_SIG2          (0x00000001 <<  9)
+#define SP_STATUS_SIG3          (0x00000001 << 10)
+#define SP_STATUS_SIG4          (0x00000001 << 11)
+#define SP_STATUS_SIG5          (0x00000001 << 12)
+#define SP_STATUS_SIG6          (0x00000001 << 13)
+#define SP_STATUS_SIG7          (0x00000001 << 14)
+
+extern pu32 CR[16];
+
+extern void SP_DMA_READ(void);
+extern void SP_DMA_WRITE(void);
+
+/*** shared states between the scalar and vector units ***/
+extern u16 get_VCO(void);
+extern u16 get_VCC(void);
+extern u8 get_VCE(void);
+extern void set_VCO(u16 VCO);
+extern void set_VCC(u16 VCC);
+extern void set_VCE(u8 VCE);
+extern i16 vce[8];
+
+extern u16 rwR_VCE(void);
+extern void rwW_VCE(u16 VCE);
+
+extern void MFC2(int rt, int vs, int e);
+extern void MTC2(int rt, int vd, int e);
+extern void CFC2(int rt, int rd);
+extern void CTC2(int rt, int rd);
+
+/*** Modern pseudo-operations (not real instructions, but nice shortcuts) ***/
+extern void ULW(int rd, u32 addr);
+extern void USW(int rs, u32 addr);
+
+/*
+ * The scalar unit controls the primary R4000 operations implementation,
+ * which inherently includes interfacing with the vector unit under COP2.
+ *
+ * Although no scalar unit operations are computational vector operations,
+ * several of them will access machine states shared with the vector unit.
+ *
+ * We will need access to the vector unit's vector register file and its
+ * vector control register file used mainly for vector select instructions.
+ */
+#include "vu/select.h"
+
+NOINLINE extern void res_S(void);
+
+extern void SP_CP0_MF(int rt, int rd);
+
+/*
+ * example syntax (basically the same for all LWC2/SWC2 ops):
+ * LTWV    $v0[0], -64($at)
+ * SBV     $v0[9], 0xFFE($0)
+ */
+typedef void(*mwc2_func)(int vt, int element, signed int offset, int base);
+
+extern mwc2_func LWC2[2 * 8*2];
+extern mwc2_func SWC2[2 * 8*2];
+
+NOINLINE void res_lsw(int vt, int element, signed int offset, int base);
+
+/*** Scalar, Coprocessor Operations (vector unit, scalar cache transfers) ***/
+extern void LBV(int vt, int element, signed int offset, int base);
+extern void LSV(int vt, int element, signed int offset, int base);
+extern void LLV(int vt, int element, signed int offset, int base);
+extern void LDV(int vt, int element, signed int offset, int base);
+extern void SBV(int vt, int element, signed int offset, int base);
+extern void SSV(int vt, int element, signed int offset, int base);
+extern void SLV(int vt, int element, signed int offset, int base);
+extern void SDV(int vt, int element, signed int offset, int base);
+
+/*
+ * Group II vector loads and stores:
+ * PV and UV (As of RCP implementation, XV and ZV are reserved opcodes.)
+ */
+extern void LPV(int vt, int element, signed int offset, int base);
+extern void LUV(int vt, int element, signed int offset, int base);
+extern void SPV(int vt, int element, signed int offset, int base);
+extern void SUV(int vt, int element, signed int offset, int base);
+
+/*
+ * Group III vector loads and stores:
+ * HV, FV, and AV (As of RCP implementation, AV opcodes are reserved.)
+ */
+extern void LHV(int vt, int element, signed int offset, int base);
+extern void LFV(int vt, int element, signed int offset, int base);
+extern void SHV(int vt, int element, signed int offset, int base);
+extern void SFV(int vt, int element, signed int offset, int base);
+
+/*
+ * Group IV vector loads and stores:
+ * QV and RV
+ */
+extern void LQV(int vt, int element, signed int offset, int base);
+extern void LRV(int vt, int element, signed int offset, int base);
+extern void SQV(int vt, int element, signed int offset, int base);
+extern void SRV(int vt, int element, signed int offset, int base);
+
+/*
+ * Group V vector loads and stores
+ * TV and SWV (As of RCP implementation, LTWV opcode was undesired.)
+ */
+extern void LTV(int vt, int element, signed int offset, int base);
+extern void SWV(int vt, int element, signed int offset, int base);
+extern void STV(int vt, int element, signed int offset, int base);
+
+NOINLINE extern void run_task(void);
+
+/*
+ * Unfortunately, SSE machine code takes up so much space in the instruction
+ * cache when populated enough in something like an interpreter switch
+ * statement, that the compiler starts looking for ways to create branches
+ * and jumps where the C code specifies none.  This complex set of macros
+ * is intended to minimize the compiler's obligation to choose doing this
+ * since SSE2 has no static shuffle operation with a variable mask operand.
+ */
+#ifdef ARCH_MIN_SSE2
+#define EXECUTE_VU() { target = *(v16 *)VR[vt]; \
+    *(v16 *)(VR[vd]) = vector_op(source, target); }
+#define EXECUTE_VU_0Q() { \
+    target = _mm_shufflehi_epi16(_mm_shufflelo_epi16(*(v16 *)VR[vt], \
+        SHUFFLE(00, 00, 02, 02)), SHUFFLE(04, 04, 06, 06)); \
+    *(v16 *)(VR[vd]) = vector_op(source, target); }
+#define EXECUTE_VU_1Q() { \
+    target = _mm_shufflehi_epi16(_mm_shufflelo_epi16(*(v16 *)VR[vt], \
+        SHUFFLE(01, 01, 03, 03)), SHUFFLE(05, 05, 07, 07)); \
+    *(v16 *)(VR[vd]) = vector_op(source, target); }
+#define EXECUTE_VU_0H() { \
+    target = _mm_shufflehi_epi16(_mm_shufflelo_epi16(*(v16 *)VR[vt], \
+        SHUFFLE(00, 00, 00, 00)), SHUFFLE(04, 04, 04, 04)); \
+    *(v16 *)(VR[vd]) = vector_op(source, target); }
+#define EXECUTE_VU_1H() { \
+    target = _mm_shufflehi_epi16(_mm_shufflelo_epi16(*(v16 *)VR[vt], \
+        SHUFFLE(01, 01, 01, 01)), SHUFFLE(05, 05, 05, 05)); \
+    *(v16 *)(VR[vd]) = vector_op(source, target); }
+#define EXECUTE_VU_2H() { \
+    target = _mm_shufflehi_epi16(_mm_shufflelo_epi16(*(v16 *)VR[vt], \
+        SHUFFLE(02, 02, 02, 02)), SHUFFLE(06, 06, 06, 06)); \
+    *(v16 *)(VR[vd]) = vector_op(source, target); }
+#define EXECUTE_VU_3H() { \
+    target = _mm_shufflehi_epi16(_mm_shufflelo_epi16(*(v16 *)VR[vt], \
+        SHUFFLE(03, 03, 03, 03)), SHUFFLE(07, 07, 07, 07)); \
+    *(v16 *)(VR[vd]) = vector_op(source, target); }
+#define EXECUTE_VU_0W() { \
+    target = _mm_shuffle_epi32(_mm_shufflelo_epi16(*(v16 *)VR[vt], \
+        SHUFFLE(00, 00, 00, 00)), SHUFFLE(0/2, 0/2, 0/2, 0/2)); \
+    *(v16 *)(VR[vd]) = vector_op(source, target); }
+#define EXECUTE_VU_1W() { \
+    target = _mm_shuffle_epi32(_mm_shufflelo_epi16(*(v16 *)VR[vt], \
+        SHUFFLE(01, 01, 01, 01)), SHUFFLE(1/2, 1/2, 1/2, 1/2)); \
+    *(v16 *)(VR[vd]) = vector_op(source, target); }
+#define EXECUTE_VU_2W() { \
+    target = _mm_shuffle_epi32(_mm_shufflelo_epi16(*(v16 *)VR[vt], \
+        SHUFFLE(02, 02, 02, 02)), SHUFFLE(2/2, 2/2, 2/2, 2/2)); \
+    *(v16 *)(VR[vd]) = vector_op(source, target); }
+#define EXECUTE_VU_3W() { \
+    target = _mm_shuffle_epi32(_mm_shufflelo_epi16(*(v16 *)VR[vt], \
+        SHUFFLE(03, 03, 03, 03)), SHUFFLE(3/2, 3/2, 3/2, 3/2)); \
+    *(v16 *)(VR[vd]) = vector_op(source, target); }
+#define EXECUTE_VU_4W() { \
+    target = _mm_shuffle_epi32(_mm_shufflehi_epi16(*(v16 *)VR[vt], \
+        SHUFFLE(04, 04, 04, 04)), SHUFFLE(4/2, 4/2, 4/2, 4/2)); \
+    *(v16 *)(VR[vd]) = vector_op(source, target); }
+#define EXECUTE_VU_5W() { \
+    target = _mm_shuffle_epi32(_mm_shufflehi_epi16(*(v16 *)VR[vt], \
+        SHUFFLE(05, 05, 05, 05)), SHUFFLE(5/2, 5/2, 5/2, 5/2)); \
+    *(v16 *)(VR[vd]) = vector_op(source, target); }
+#define EXECUTE_VU_6W() { \
+    target = _mm_shuffle_epi32(_mm_shufflehi_epi16(*(v16 *)VR[vt], \
+        SHUFFLE(06, 06, 06, 06)), SHUFFLE(6/2, 6/2, 6/2, 6/2)); \
+    *(v16 *)(VR[vd]) = vector_op(source, target); }
+#define EXECUTE_VU_7W() { \
+    target = _mm_shuffle_epi32(_mm_shufflehi_epi16(*(v16 *)VR[vt], \
+        SHUFFLE(07, 07, 07, 07)), SHUFFLE(7/2, 7/2, 7/2, 7/2)); \
+    *(v16 *)(VR[vd]) = vector_op(source, target); }
+#else
+#define EXECUTE_VU() { \
+    vector_copy(target, VR[vt]); SHUFFLE_VECTOR(target, 0x0); \
+    vector_op(source, target); vector_copy(VR[vd], V_result); }
+#define EXECUTE_VU_0Q() { \
+    vector_copy(target, VR[vt]); SHUFFLE_VECTOR(target, 0x2); \
+    vector_op(source, target); vector_copy(VR[vd], V_result); }
+#define EXECUTE_VU_1Q() { \
+    vector_copy(target, VR[vt]); SHUFFLE_VECTOR(target, 0x3); \
+    vector_op(source, target); vector_copy(VR[vd], V_result); }
+#define EXECUTE_VU_0H() { \
+    vector_copy(target, VR[vt]); SHUFFLE_VECTOR(target, 0x4); \
+    vector_op(source, target); vector_copy(VR[vd], V_result); }
+#define EXECUTE_VU_1H() { \
+    vector_copy(target, VR[vt]); SHUFFLE_VECTOR(target, 0x5); \
+    vector_op(source, target); vector_copy(VR[vd], V_result); }
+#define EXECUTE_VU_2H() { \
+    vector_copy(target, VR[vt]); SHUFFLE_VECTOR(target, 0x6); \
+    vector_op(source, target); vector_copy(VR[vd], V_result); }
+#define EXECUTE_VU_3H() { \
+    vector_copy(target, VR[vt]); SHUFFLE_VECTOR(target, 0x7); \
+    vector_op(source, target); vector_copy(VR[vd], V_result); }
+#define EXECUTE_VU_0W() { \
+    vector_copy(target, VR[vt]); SHUFFLE_VECTOR(target, 0x8); \
+    vector_op(source, target); vector_copy(VR[vd], V_result); }
+#define EXECUTE_VU_1W() { \
+    vector_copy(target, VR[vt]); SHUFFLE_VECTOR(target, 0x9); \
+    vector_op(source, target); vector_copy(VR[vd], V_result); }
+#define EXECUTE_VU_2W() { \
+    vector_copy(target, VR[vt]); SHUFFLE_VECTOR(target, 0xA); \
+    vector_op(source, target); vector_copy(VR[vd], V_result); }
+#define EXECUTE_VU_3W() { \
+    vector_copy(target, VR[vt]); SHUFFLE_VECTOR(target, 0xB); \
+    vector_op(source, target); vector_copy(VR[vd], V_result); }
+#define EXECUTE_VU_4W() { \
+    vector_copy(target, VR[vt]); SHUFFLE_VECTOR(target, 0xC); \
+    vector_op(source, target); vector_copy(VR[vd], V_result); }
+#define EXECUTE_VU_5W() { \
+    vector_copy(target, VR[vt]); SHUFFLE_VECTOR(target, 0xD); \
+    vector_op(source, target); vector_copy(VR[vd], V_result); }
+#define EXECUTE_VU_6W() { \
+    vector_copy(target, VR[vt]); SHUFFLE_VECTOR(target, 0xE); \
+    vector_op(source, target); vector_copy(VR[vd], V_result); }
+#define EXECUTE_VU_7W() { \
+    vector_copy(target, VR[vt]); SHUFFLE_VECTOR(target, 0xF); \
+    vector_op(source, target); vector_copy(VR[vd], V_result); }
+#endif
+
+#endif
---
+++ jni/mupen64plus-rsp-cxd4/vu/vu.h
@@ -0,0 +1,261 @@
+/******************************************************************************\
+* Project:  MSP Emulation Layer for Vector Unit Computational Operations       *
+* Authors:  Iconoclast                                                         *
+* Release:  2014.12.13                                                         *
+* License:  CC0 Public Domain Dedication                                       *
+*                                                                              *
+* To the extent possible under law, the author(s) have dedicated all copyright *
+* and related and neighboring rights to this software to the public domain     *
+* worldwide. This software is distributed without any warranty.                *
+*                                                                              *
+* You should have received a copy of the CC0 Public Domain Dedication along    *
+* with this software.                                                          *
+* If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.             *
+\******************************************************************************/
+#ifndef _VU_H_
+#define _VU_H_
+
+#ifdef ARCH_MIN_SSE2
+#include <emmintrin.h>
+#endif
+
+#include "../my_types.h"
+
+#define N       8
+/* N:  number of processor elements in SIMD processor */
+
+/*
+ * We are going to need this for vector operations doing scalar things.
+ * The divides and VSAW need bit-wise information from the instruction word.
+ */
+extern u32 inst;
+
+/*
+ * RSP virtual registers (of vector unit)
+ * The most important are the 32 general-purpose vector registers.
+ * The correct way to accurately store these is using big-endian vectors.
+ *
+ * For ?WC2 we may need to do byte-precision access just as directly.
+ * This is amended by using the `VU_S` and `VU_B` macros defined in `rsp.h`.
+ */
+ALIGNED extern i16 VR[32][N];
+
+/*
+ * The RSP accumulator is a vector of 3 48-bit integers.  Nearly all of the
+ * vector operations access it, but it's for multiply-accumulate operations.
+ *
+ * Access dimensions would be VACC[8][3] but are inverted for SIMD benefits.
+ */
+ALIGNED extern i16 VACC[3][N];
+
+/*
+ * When compiling without SSE2, we need to use a pointer to a destination
+ * vector instead of an XMM register in the return slot of the function.
+ * The vector "result" register will be emulated to serve this pointer
+ * as a shared global rather than the return slot of a function call.
+ */
+#ifndef ARCH_MIN_SSE2
+ALIGNED extern i16 V_result[N];
+#endif
+
+/*
+ * accumulator-indexing macros
+ */
+#define HI      00
+#define MD      01
+#define LO      02
+
+#define VACC_L      (VACC[LO])
+#define VACC_M      (VACC[MD])
+#define VACC_H      (VACC[HI])
+
+#define ACC_L(i)    (VACC_L)[i]
+#define ACC_M(i)    (VACC_M)[i]
+#define ACC_H(i)    (VACC_H)[i]
+
+#ifdef ARCH_MIN_SSE2
+typedef __m128i v16;
+#else
+typedef pi16 v16;
+#endif
+
+#ifdef ARCH_MIN_SSE2
+#define VECTOR_OPERATION    v16
+#else
+#define VECTOR_OPERATION    void
+#endif
+#define VECTOR_EXTERN       extern VECTOR_OPERATION
+
+NOINLINE extern void message(const char* body);
+
+VECTOR_EXTERN (*COP2_C2[8*7 + 8])(v16, v16);
+
+#ifdef ARCH_MIN_SSE2
+
+#define vector_copy(vd, vs) { \
+    *(v16 *)(vd) = *(v16 *)(vs); }
+#define vector_wipe(vd) { \
+    (vd) = _mm_xor_si128(vd, vd); }
+#define vector_fill(vd) { \
+    (vd) = _mm_cmpeq_epi16(vd, vd); }
+
+#define vector_and(vd, vs) { \
+    (vd) = _mm_and_si128    (vd, vs); }
+#define vector_or(vd, vs) { \
+    (vd) = _mm_or_si128     (vd, vs); }
+#define vector_xor(vd, vs) { \
+    (vd) = _mm_xor_si128    (vd, vs); }
+
+/*
+ * Every competent vector unit should have at least two vector comparison
+ * operations:  EQ and LT/GT.  (MMX makes us say GT; SSE's LT is just a GT.)
+ *
+ * Default examples when compiling for the x86 SSE2 architecture below.
+ */
+#define vector_cmplt(vd, vs) { \
+    (vd) = _mm_cmplt_epi16  (vd, vs); }
+#define vector_cmpeq(vd, vs) { \
+    (vd) = _mm_cmpeq_epi16  (vd, vs); }
+#define vector_cmpgt(vd, vs) { \
+    (vd) = _mm_cmpgt_epi16  (vd, vs); }
+
+#else
+
+#define vector_copy(vd, vs) { \
+    (vd)[0] = (vs)[0]; \
+    (vd)[1] = (vs)[1]; \
+    (vd)[2] = (vs)[2]; \
+    (vd)[3] = (vs)[3]; \
+    (vd)[4] = (vs)[4]; \
+    (vd)[5] = (vs)[5]; \
+    (vd)[6] = (vs)[6]; \
+    (vd)[7] = (vs)[7]; \
+}
+#define vector_wipe(vd) { \
+    (vd)[0] =  0x0000; \
+    (vd)[1] =  0x0000; \
+    (vd)[2] =  0x0000; \
+    (vd)[3] =  0x0000; \
+    (vd)[4] =  0x0000; \
+    (vd)[5] =  0x0000; \
+    (vd)[6] =  0x0000; \
+    (vd)[7] =  0x0000; \
+}
+#define vector_fill(vd) { \
+    (vd)[0] = ~0x0000; \
+    (vd)[1] = ~0x0000; \
+    (vd)[2] = ~0x0000; \
+    (vd)[3] = ~0x0000; \
+    (vd)[4] = ~0x0000; \
+    (vd)[5] = ~0x0000; \
+    (vd)[6] = ~0x0000; \
+    (vd)[7] = ~0x0000; \
+}
+#define vector_and(vd, vs) { \
+    (vd)[0] &= (vs)[0]; \
+    (vd)[1] &= (vs)[1]; \
+    (vd)[2] &= (vs)[2]; \
+    (vd)[3] &= (vs)[3]; \
+    (vd)[4] &= (vs)[4]; \
+    (vd)[5] &= (vs)[5]; \
+    (vd)[6] &= (vs)[6]; \
+    (vd)[7] &= (vs)[7]; \
+}
+#define vector_or(vd, vs) { \
+    (vd)[0] |= (vs)[0]; \
+    (vd)[1] |= (vs)[1]; \
+    (vd)[2] |= (vs)[2]; \
+    (vd)[3] |= (vs)[3]; \
+    (vd)[4] |= (vs)[4]; \
+    (vd)[5] |= (vs)[5]; \
+    (vd)[6] |= (vs)[6]; \
+    (vd)[7] |= (vs)[7]; \
+}
+#define vector_xor(vd, vs) { \
+    (vd)[0] ^= (vs)[0]; \
+    (vd)[1] ^= (vs)[1]; \
+    (vd)[2] ^= (vs)[2]; \
+    (vd)[3] ^= (vs)[3]; \
+    (vd)[4] ^= (vs)[4]; \
+    (vd)[5] ^= (vs)[5]; \
+    (vd)[6] ^= (vs)[6]; \
+    (vd)[7] ^= (vs)[7]; \
+}
+
+#define vector_cmplt(vd, vs) { \
+    (vd)[0] = ((vd)[0] < (vs)[0]) ? ~0x0000 :  0x0000; \
+    (vd)[0] = ((vd)[1] < (vs)[1]) ? ~0x0000 :  0x0000; \
+    (vd)[0] = ((vd)[2] < (vs)[2]) ? ~0x0000 :  0x0000; \
+    (vd)[0] = ((vd)[3] < (vs)[3]) ? ~0x0000 :  0x0000; \
+    (vd)[0] = ((vd)[4] < (vs)[4]) ? ~0x0000 :  0x0000; \
+    (vd)[0] = ((vd)[5] < (vs)[5]) ? ~0x0000 :  0x0000; \
+    (vd)[0] = ((vd)[6] < (vs)[6]) ? ~0x0000 :  0x0000; \
+    (vd)[0] = ((vd)[7] < (vs)[7]) ? ~0x0000 :  0x0000; \
+}
+#define vector_cmpeq(vd, vs) { \
+    (vd)[0] = ((vd)[0] == (vs)[0]) ? ~0x0000 :  0x0000; \
+    (vd)[0] = ((vd)[1] == (vs)[1]) ? ~0x0000 :  0x0000; \
+    (vd)[0] = ((vd)[2] == (vs)[2]) ? ~0x0000 :  0x0000; \
+    (vd)[0] = ((vd)[3] == (vs)[3]) ? ~0x0000 :  0x0000; \
+    (vd)[0] = ((vd)[4] == (vs)[4]) ? ~0x0000 :  0x0000; \
+    (vd)[0] = ((vd)[5] == (vs)[5]) ? ~0x0000 :  0x0000; \
+    (vd)[0] = ((vd)[6] == (vs)[6]) ? ~0x0000 :  0x0000; \
+    (vd)[0] = ((vd)[7] == (vs)[7]) ? ~0x0000 :  0x0000; \
+}
+#define vector_cmpgt(vd, vs) { \
+    (vd)[0] = ((vd)[0] > (vs)[0]) ? ~0x0000 :  0x0000; \
+    (vd)[0] = ((vd)[1] > (vs)[1]) ? ~0x0000 :  0x0000; \
+    (vd)[0] = ((vd)[2] > (vs)[2]) ? ~0x0000 :  0x0000; \
+    (vd)[0] = ((vd)[3] > (vs)[3]) ? ~0x0000 :  0x0000; \
+    (vd)[0] = ((vd)[4] > (vs)[4]) ? ~0x0000 :  0x0000; \
+    (vd)[0] = ((vd)[5] > (vs)[5]) ? ~0x0000 :  0x0000; \
+    (vd)[0] = ((vd)[6] > (vs)[6]) ? ~0x0000 :  0x0000; \
+    (vd)[0] = ((vd)[7] > (vs)[7]) ? ~0x0000 :  0x0000; \
+}
+
+#endif
+
+/*
+ * Considering that almost all of the computational vector unit operations
+ * concern a shuffled, halfword-swapped target vector, a centralized method
+ * for shuffling said vector into a new vector temporary register,
+ * ST = VR[vt], should be pretty convenient.
+ */
+INLINE VECTOR_EXTERN SHUFFLE_VECTOR(v16 VD, const int e);
+
+/*
+ * Many vector units have pairs of "vector condition flags" registers.
+ * In SGI's vector unit implementation, these are denoted as the
+ * "vector control registers" under coprocessor 2.
+ *
+ * VCF-0 is the carry-out flags register:  $vco.
+ * VCF-1 is the compare code flags register:  $vcc.
+ * VCF-2 is the compare extension flags register:  $vce.
+ * There is no fourth RSP flags register.
+ */
+extern u16 VCO;
+extern u16 VCC;
+extern u8 VCE;
+
+ALIGNED extern i16 ne[8];
+ALIGNED extern i16 co[8];
+ALIGNED extern i16 clip[8];
+ALIGNED extern i16 comp[8];
+ALIGNED extern i16 vce[8];
+
+extern u16 get_VCO(void);
+extern u16 get_VCC(void);
+extern u8 get_VCE(void);
+
+extern void set_VCO(u16 VCO);
+extern void set_VCC(u16 VCC);
+extern void set_VCE(u8 VCE);
+
+/*
+ * shuffling convenience macros for Intel SIMD
+ * An 8-bit shuffle imm. of SHUFFLE(0, 1, 2, 3) should be a null operation.
+ */
+#define B(x)    ((x) & 3)
+#define SHUFFLE(a,b,c,d)    ((B(d)<<6) | (B(c)<<4) | (B(b)<<2) | (B(a)<<0))
+
+#endif
